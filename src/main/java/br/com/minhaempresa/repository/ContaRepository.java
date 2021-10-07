ee6f7d1b0d801d0d662666d23fc0708b922bc9d4
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-07 19:14:42 -0300

    Ao invés de chamar o método getConexao() em cada método de consulta, depósito, transferência e saque, eu crio um atributo na classe do repository "ContaRepository" nesse caso, e quando precisar da conexão, uso o atributo

--- src/main/java/br/com/minhaempresa/repository/ContaRepository.java
+++ src/main/java/br/com/minhaempresa/repository/ContaRepository.java
@@ -4,13 +4,13 @@ import javax.persistence.EntityManager;
 
 public class ContaRepository {
 
+    private EntityManager entityManager = Conexao.getConexao();
+
     public double consultarSaldo() {
-        EntityManager em = Conexao.getConexao();
 
         return 0;
     }
 
     public void inserirDeposito() {
-        EntityManager em = Conexao.getConexao();
     }
 }
