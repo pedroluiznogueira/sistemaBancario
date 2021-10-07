c3b6e951efb5259314dff64febf8c51c3fcb05d2
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-07 12:35:07 -0300

    Explorando e estruturando de uma melhor forma o servlet consultar-saldo

--- src/main/java/br/com/minhaempresa/controller/ConsultarSaldoController.java
+++ src/main/java/br/com/minhaempresa/controller/ConsultarSaldoController.java
@@ -11,18 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 // declaração do servlet extendendo a classe HttpServlet
-@WebServlet(urlPatterns = "/ConsultarSaldo")
+@WebServlet(urlPatterns = "/consultar-saldo")
 public class ConsultarSaldoController extends HttpServlet {
 
     // usando doGet pois precisamos vai ser "pegado" algo do servidor e não trazido algo para o servidor
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // criando um cliente para passar como parâmetro da conta que vai ser consultada
-        Cliente clienteUm = new Cliente("pedro", "luiz");
-        // criando uma conta corrente
-        ContaCorrente contaCorrente = new ContaCorrente(clienteUm);
-
-        // printando o saldo da conta usando o método consultarSaldo
-        resp.getWriter().println(contaCorrente.consultarSaldo());
+        resp.getWriter().println("servlet funcionando");
     }
 }
