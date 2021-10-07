8aee00708b983600cdd0c5b3bc2498f16848d6f3
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-07 01:02:12 -0300

    Adicionando o projeto do SistemaFinanceiro como dependência á esse projeto, instalando-o como um jar(java archive) no meu repositorio local da minha máquina e importando-o nas dependências no arquivo pom.xml

---
+++ src/main/java/br/com/minhaempresa/Teste.java
@@ -0,0 +1,11 @@
+package br.com.minhaempresa;
+
+import br.com.minhaempresa.domain.ContaCorrente;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        ContaCorrente cc = new ContaCorrente();
+    }
+}
