a64fb2b010b19ee53d929b99e5929000adb029d6
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-07 18:43:18 -0300

    Não é interessante eu repetir a instanciação da classe Conexao e chamar o método getConexao()

--- src/main/java/br/com/minhaempresa/repository/ContaRepository.java
+++ src/main/java/br/com/minhaempresa/repository/ContaRepository.java
@@ -1,11 +1,24 @@
 package br.com.minhaempresa.repository;
 
+import javax.persistence.EntityManager;
+
 public class ContaRepository {
 
     public double consultarSaldo() {
+
+        // instanciando a classe que faz a conexao
+        Conexao conexao = new Conexao();
+        // usando o métood que faz a conexao
+        EntityManager em = conexao.getConexao();
+
         return 0;
     }
 
     public void inserirDeposito() {
+
+        // instanciando a classe que faz a conexao
+        Conexao conexao = new Conexao();
+        // usando o métood que faz a conexao
+        EntityManager em = conexao.getConexao();
     }
 }
