9f38707b9db86955f7dac5df39e37acd2c84ae94
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-07 11:45:02 -0300

    Usando doGet para consultar o saldo

--- src/main/java/br/com/minhaempresa/controller/ConsultarSaldoController.java
+++ src/main/java/br/com/minhaempresa/controller/ConsultarSaldoController.java
@@ -13,4 +13,16 @@ import java.io.IOException;
 // declaração do servlet extendendo a classe HttpServlet
 @WebServlet(urlPatterns = "/ConsultarSaldo")
 public class ConsultarSaldoController extends HttpServlet {
+
+    // usando doGet pois precisamos vai ser "pegado" algo do servidor e não trazido algo para o servidor
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // criando um cliente para passar como parâmetro da conta que vai ser consultada
+        Cliente clienteUm = new Cliente("pedro", "luiz");
+        // criando uma conta corrente
+        ContaCorrente contaCorrente = new ContaCorrente(clienteUm);
+
+        // printando o saldo da conta usando o método consultarSaldo
+        resp.getWriter().println(contaCorrente.consultarSaldo());
+    }
 }
