75707dfa3556c9817552907cff07fc6b6ef3204c
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-08 16:18:43 -0300

    Mapeando a classe abstrata Conta, que não será uma entidade no banco de dados

--- src/main/java/br/com/minhaempresa/domain/Conta.java
+++ src/main/java/br/com/minhaempresa/domain/Conta.java
@@ -2,23 +2,33 @@ package br.com.minhaempresa.domain;
 
 import br.com.minhaempresa.exceptions.SaldoInsuficienteException;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+// uma classe abstrata não se torna uma entidade completa da aplicação, com essa annotation estou mapeando como uma super classe, que não será uma tabela
+@MappedSuperclass
 public abstract class Conta {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String numero;
     private double saldo;
-    private Cliente titular;
+    // private Cliente titular;
 
     public Conta(Cliente titular) {
         this.saldo = 0;
         this.numero = "1";
-        this.titular = titular;
+        // this.titular = titular;
     }
 
     public Conta(Cliente titular, String conta) {
         this.saldo = 0;
         this.numero =  numero;
-        this.titular = titular;
+        // this.titular = titular;
     }
 
 
@@ -38,13 +48,13 @@ public abstract class Conta {
         this.saldo = saldo;
     }
 
-    public Cliente getTitular() {
-        return titular;
-    }
+    // public Cliente getTitular() {
+        // return titular;
+    // }
 
-    public void setTitular(Cliente titular) {
-        this.titular = titular;
-    }
+    // public void setTitular(Cliente titular) {
+        // this.titular = titular;
+    // }
 
     public void sacar(double valor) throws IllegalArgumentException, SaldoInsuficienteException {
         System.out.println("Tentativa de saque na conta" + this.numero + " no valor de: " + valor);
